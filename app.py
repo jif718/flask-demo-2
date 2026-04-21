8353f204cbb20ffd15ec9fbe5ea74fa727b4905d
YIFAN JIA <yifanjia@macbook.lan>
2026-04-21 17:42:24 +0800

    update version

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask Demo v1.2\n"
+    return "Hello from Flask Demo v1.3\n"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
